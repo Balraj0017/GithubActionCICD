1de5ea4dd6c7a4b3ba326c89b70be02b8dfabff2
balraj0017 <balrajsabharwal865@gmail.com>
2024-11-15 12:12:09 +0530

    adding the java project

---
+++ demo-springgithubactions/src/main/java/com/example/demo_springgithubactions/DemoSpringgithubactionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_springgithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringgithubactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringgithubactionsApplication.class, args);
+	}
+
+}
